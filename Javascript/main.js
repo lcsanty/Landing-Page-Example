e891329ce2e7d7a7b5022e00960cbac979e48d90
Liam Santy <liamsanty@Liams-iMac.local>
2018-08-09 17:16:53 +0100

    finished page

---
+++ Javascript/main.js
@@ -0,0 +1,17 @@
+let button = document.querySelector(".icon");
+let lock = document.body;
+let mobile = document.querySelector(".mobile-menu");
+let mobMenu = document.querySelector(".mobile-list");
+button.addEventListener("click", () => {
+  if (button.className === "icon") {
+    button.className += " active";
+    lock.className = "lock";
+    mobile.className += " show";
+    mobMenu.className += " show";
+  } else {
+    button.className = "icon";
+    lock.className = "";
+    mobile.className = "mobile-menu";
+    mobMenu.className = "mobile-list";
+  }
+});
